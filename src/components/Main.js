9cbf81eacadd4026307554fa4c855aad27180696
Rebeca Casa <rebi_britto@hotmail.com>
2019-03-26 12:46:10 +0100

    rebeca profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,51 +16,54 @@ class Main extends React.Component {
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Intro</h2>
           <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <p>2 years working in Michelin Star restaurants taught me a lot -
+              how to deal with intense stress, tight deadlines & how to work in a team. <br/>
+                  <br/>
+                  Technology has always interested me & I love to challenge myself, <br/>
+                  so started experimenting with programming. I’m now looking at working as <br/>
+                  a full-stack developer as it fits all of my career requirements: challenging work, <br/>
+                  working with a team, having an impact - it’s also nice to work relatively sociable <br/>
+                  hours after being in a kitchen for a few years!</p>
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <p><strong>Owner, Baersilien Cafe-Berlin, Germany </strong> <br/>
+            01/2018- Today<br/>
+            Responsible for the menu, marketing, events & finances of a family business in Berlin,
+            requiring creativity & application of my business skills in a new environment to grow revenue. <br/>
+            <strong>Chef de Partie, L’ecrivain Restaurant - Dublin, Ireland </strong>  <br/>
+            09/2015 - 04/2017<br/>
+            Delivering Michelin-star quality meals in a renowned Dublin restaurant in
+            a variety of roles & teams.<br/>
+            <strong>Culinary Extern, Thornton’s Restaurant - Dublin, Ireland</strong><br/>
+            04/2015 - 09/2015<br/>
+            Working across different teams new, complex dishes delivered to a high standard under intense time pressure.<br/>
+            <strong>Finance Manager, Transrebeca - Sao Paulo, Brazil</strong><br/>
+            01/2009 - 12/2012<br/>
+            Joint management of company finances  in a family company in Brazil.
+            Responsible for the company's income & outgoings.</p>
           {close}
         </article>
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <h2 className="major">Projects</h2>
+            <ul>
+              <li><a href="http://www.pimpyourprint.tech/">Pimp your print</a></li>
+              <li><a href="http://toylandia.herokuapp.com/">Toylandia</a></li>
+              <li><a href="https://rebeca-cocktails.herokuapp.com/">Mr. Cocktails</a></li>
+            </ul>
           {close}
         </article>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
           <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="mailto:rebecabscs@gmail.com" className="icon fa-envelope"><span className="label">Email</span></a></li>
+            <li><a href="https://www.linkedin.com/in/rebeca-casa-859553176/" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
+            <li><a href="https://github.com/Rebecasa" className="icon fa-github"><span className="label">GitHub</span></a></li>
           </ul>
           {close}
         </article>
