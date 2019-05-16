c77ee54c35efd1ac90408335347c141c32be005c
Rebeca Casa <rebi_britto@hotmail.com>
2019-05-16 15:39:24 +0200

    trying to create a new page

---
+++ src/pages/music.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const Music = () => (
+  <Layout>
+   <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/253572245&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+  </Layout>
+)
+
+export default Music
