536e937d2cfe8b6428566deb1b14bb2e7c9b1b72
Rebeca Casa <rebi_britto@hotmail.com>
2019-05-16 15:54:38 +0200

    added breaks

--- src/pages/music.js
+++ src/pages/music.js
@@ -13,11 +13,11 @@ const Music = () => (
   <p>
   As somebody who has developed very quickly under working closely with colleagues much
 more senior than myself (in this case, two Michelin star restaurants as a young, ambitious chef) this opportunity was a really exciting prospect for me.
-
+<br/>
 Although I have developed effective skills in JavaScript, Rails & React, I am humble enough to recognise the fact that I still have a lot to learn as a developer, so would relish the opportunity to really work closely with more senior developers
-
+<br/>
 Diverse projects, paired programming & a clear prioritisation of helping team members develop is a huge attraction to me & an opportunity I would love to take up.
-
+<br/>
 My experience at the highest level in cuisine - as well as the ability to work under intense
 pressure, act calm & be resourceful, as well as work very closely as part of a time - is a unique aspect of my experience that I could bring to the team.
 </p></div>
