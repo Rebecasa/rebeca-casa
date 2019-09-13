8421231f48df3ba6ba725440e42f96b5a78bcc96
Rebeca Casa <rebi_britto@hotmail.com>
2019-09-13 11:12:23 +0200

    updated work field

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,48 +16,78 @@ class Main extends React.Component {
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Intro</h2>
           <p>2 years working in Michelin Star restaurants taught me a lot -
-              how to deal with intense stress, tight deadlines & how to work in a team. <br/>
+              how to deal with intense stress, tight deadlines & how to work in a team.
+              <br/>
+
                   <br/>
-                  Technology has always interested me & I love to challenge myself, <br/>
-                  so started experimenting with programming. I’m now looking at working as <br/>
-                  a full-stack developer as it fits all of my career requirements: challenging work, <br/>
-                  working with a team, having an impact - it’s also nice to work relatively sociable <br/>
+                  Technology has always interested me & I love to challenge myself,
+                  <br/>
+                  so started experimenting with programming. I’m now looking at working as
+                  <br/>
+                  a full-stack developer as it fits all of my career requirements: challenging work,
+                  <br/>
+                  working with a team, having an impact - it’s also nice to work relatively sociable
+                  <br/>
                   hours after being in a kitchen for a few years!</p>
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
-           <p><strong>Front end developer - Internship, Landaio-Berlin, Germany </strong> <br/>
-            06/2019 -09/2019<br/>
+           <p><strong>Front end developer - Internship, Landaio-Berlin, Germany </strong>
+            <br/>
+            06/2019 -09/2019
+            <br/>
             At Blindside, I learnt how to effectively collaborate on a complex, large-scale engineering project,
             working closely with the CTO and other experienced developers on a React health app.
             Specifically, I contributed to several pull requests each day, mainly focused on user
-            stories related to React, SCSS & some Redux.<br/>
-            <strong>Internship, Scribe App-Berlin, Germany </strong> <br/>
-            12/2018 - 02/2019<br/><p>
-            <strong>Front end developer - Internship, Landaio-Berlin, Germany </strong> <br/>
-            03/2019 -05/2019<br/>
+            stories related to React, SCSS & some Redux.
+            <br/>
+            <br/>
+            <strong>Front end developer - Internship, Landaio-Berlin, Germany </strong>
+            <br/>
+            03/2019 -05/2019
+            <br/>
             Technical lead in an early stage startup, working closely with the
-            founders to set up data analytics, A/B testing & a new Reactbased web platform.<br/>
-            <strong>Front end developer - Internship, Scribe App-Berlin, Germany </strong> <br/>
-            12/2018 - 02/2019<br/>
+            founders to set up data analytics, A/B testing & a new Reactbased web platform.
+            <br/>
+            <br/>
+            <strong>Front end developer - Internship, Scribe App-Berlin, Germany </strong>
+            <br/>
+            12/2018 - 02/2019
+            <br/>
             I took the initiative to work with the founders on a new React
             project to gain some product development & React experience
-            after finishing the Le Wagon programming course.<br/>
-            <strong>Owner, Baersilien Cafe-Berlin, Germany </strong> <br/>
-            01/2018-01/2019<br/>
+            after finishing the Le Wagon programming course.
+            <br/>
+            <br/>
+            <strong>Owner, Baersilien Cafe-Berlin, Germany </strong>
+            <br/>
+            01/2018-01/2019
+            <br/>
             Responsible for the menu, marketing, events & finances of a family business in Berlin,
-            requiring creativity & application of my business skills in a new environment to grow revenue. <br/>
-            <strong>Chef de Partie, L’ecrivain Restaurant - Dublin, Ireland </strong>  <br/>
-            09/2015 - 04/2017<br/>
+            requiring creativity & application of my business skills in a new environment to grow revenue.
+            <br/>
+            <br/>
+            <strong>Chef de Partie, L’ecrivain Restaurant - Dublin, Ireland </strong>
+            <br/>
+            09/2015 - 04/2017
+            <br/>
             Delivering Michelin-star quality meals in a renowned Dublin restaurant in
-            a variety of roles & teams.<br/>
-            <strong>Culinary Extern, Thornton’s Restaurant - Dublin, Ireland</strong><br/>
-            04/2015 - 09/2015<br/>
-            Working across different teams new, complex dishes delivered to a high standard under intense time pressure.<br/>
-            <strong>Finance Manager, Transrebeca - Sao Paulo, Brazil</strong><br/>
-            01/2009 - 12/2012<br/>
+            a variety of roles & teams.
+            <br/>
+            <br/>
+            <strong>Culinary Extern, Thornton’s Restaurant - Dublin, Ireland</strong>
+            <br/>
+            04/2015 - 09/2015
+            <br/>
+            Working across different teams new, complex dishes delivered to a high standard under intense time pressure.
+            <br/>
+            <br/>
+            <strong>Finance Manager, Transrebeca - Sao Paulo, Brazil</strong>
+            <br/>
+            01/2009 - 12/2012
+            <br/>
             Joint management of company finances  in a family company in Brazil.
             Responsible for the company's income & outgoings.</p>
           {close}
